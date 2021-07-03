054779d956f79d54561727bc6e2b0fcde2080ebb
Kumar Saurabh <kumarsaurabh471990@gmail.com>
2021-07-03 14:43:28 +0530

    added CORS

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import userRouter from "./routers/userRouters.js";
 import productRouter from "./routers/productRouter.js";
 import orderRouter from "./routers/orderRouter.js";
 
+
+ 
+
 dotenv.config();
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/shoper", {
